722a70471ed198a1e947712daf4820552c6468ff
abhishekliberty <105413723+abhishekliberty@users.noreply.github.com>
2023-09-25 10:17:30 +0530

    Bug fixes

--- contracts/LibrtyTournament.sol
+++ contracts/LibrtyTournament.sol
@@ -3,8 +3,6 @@ pragma solidity ^0.8.9;
 
 import { IERC20 } from "./interface/IERC20.sol";
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
 
 //must be converted to ownable
 contract LibrtyTournament {
@@ -29,10 +27,10 @@ contract LibrtyTournament {
     }
     
     Tournament public current_tournament;
-    string[] public winners;
+    string[] public winners; // storing team ids
     mapping(string => Team) public teams;
 
-    address public LIBRTY_OPERATOR = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    address public LIBRTY_OPERATOR = 0x424Fc8BdcFFF8f1df6d7c2a4fCF4E1D7Db8929f8;
     address public owner;
     TournamentStatus public tournament_status;
 
@@ -50,7 +48,7 @@ contract LibrtyTournament {
 
     /** Create a new tournament */
     constructor(string memory _tournament_details_hash, address token, address _organiser, uint256 amount, uint256 team_size, uint256[] memory _prize_amount) {
-        current_tournament = Tournament(_tournament_details_hash, msg.sender, token, amount, team_size, _prize_amount);
+        current_tournament = Tournament(_tournament_details_hash, _organiser, token, amount, team_size, _prize_amount);
         owner = _organiser;
         tournament_status = TournamentStatus.CREATED;
     }
@@ -58,12 +56,13 @@ contract LibrtyTournament {
     /** Register Captain as a team member and decide the prize split */
     function registerTeam(uint256 split, Player memory captain, string memory team_id) public onlyLibrty {
         require(tournament_status == TournamentStatus.CREATED, "Only created tournaments can do a split");
-        teams[team_id].split = split;
+        teams[team_id].split = split; // 30
         teams[team_id].players.push(captain);
     }
 
     /** Add players to the team */
     function addPlayerToTeam(Player memory player, string memory team_id) public onlyLibrty{
+        require(teams[team_id].players.length > 0, "Only registerd team can add players");
         teams[team_id].players.push(player);
     }
 
@@ -73,11 +72,13 @@ contract LibrtyTournament {
 
     /** Add players to the team */
     function removePlayerFromTeam(uint256 index, string memory team_id) public onlyLibrty{
+        require(teams[team_id].players.length > 0, "Only registerd team can remove players");
         delete teams[team_id].players[index];
     }
 
     /** Del team */
     function deleteTeam(string memory team_id) public onlyLibrty{
+        require(teams[team_id].players.length > 0, "Only registerd team can be deleted");
         delete teams[team_id];
     }
 
@@ -99,24 +100,27 @@ contract LibrtyTournament {
 
     function approveWinnersList() public onlyOwner{
         require(tournament_status == TournamentStatus.WINNER_LIST_SUBMITTED, "Winners are not submitted yet");
-        tournament_status = TournamentStatus.PRIZE_DISTRIBUTED;
 
         IERC20 tokenInstance = IERC20(current_tournament.token);
 
         for(uint256 i = 0; i < winners.length; i++){
             Team memory winningTeam = teams[winners[i]];
             uint256 totalPrizeForTeam = current_tournament.prize_amount[i];
-            uint256 captainSplit = (winningTeam.split * totalPrizeForTeam) / 100;
-            tokenInstance.transfer(winningTeam.players[0].wallet, captainSplit);
+            uint256 captainSplit = (winningTeam.split * totalPrizeForTeam) / 100; // 30% ( 3 70%)
+            if(winningTeam.players[0].wallet != address(0)){
+                tokenInstance.transfer(winningTeam.players[0].wallet, captainSplit);
+            }
 
             uint256 teamMemberSplit = (totalPrizeForTeam - captainSplit) / (current_tournament.team_size - 1); // removed captain from team size
 
-            for(uint256 j = 0; j < winningTeam.players.length; j++){
+            for(uint256 j = 1; j < winningTeam.players.length; j++){
                 if(winningTeam.players[j].wallet != address(0)){
                     tokenInstance.transfer(winningTeam.players[j].wallet, teamMemberSplit);
                 }
             }
         }
+
+        tournament_status = TournamentStatus.PRIZE_DISTRIBUTED;
     }
 
     function rejectWinnersList() public onlyOwner{
